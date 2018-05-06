46afd131d618ecb37d9ea07e47d1a90dc8a5e3b7
Sara Swann <saramswann18@gmail.com>
2018-05-06 13:48:03 -0400

    change line-chart CSS selectors

--- line.js
+++ line.js
@@ -1,4 +1,7 @@
-var svg = d3.select("svg"),
+var svg = d3.select("#dummy-line-chart-container")
+            .append('svg')
+            .attr('width', 600)
+            .attr('height', 600),
     margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom;
